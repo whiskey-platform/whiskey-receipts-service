32c39e987daec8f6a2ae76300350006d22b4cac2
Matthew Wyskiel <developer@mattwyskiel.com>
2023-04-08 00:31:16 -0400

    Use nodejs 18

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,23 @@
-import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack";
+import { SSTConfig } from 'sst';
+import { API } from './stacks/MyStack';
 
 export default {
   config(_input) {
     return {
-      name: "whiskey-receipts-service",
-      region: "us-east-1",
+      name: 'whiskey-receipts-service',
+      region: 'us-east-1',
     };
   },
   stacks(app) {
+    app.setDefaultFunctionProps({
+      runtime: 'nodejs18.x',
+      nodejs: {
+        esbuild: {
+          external: !app.local ? ['@aws-sdk/*'] : undefined,
+        },
+      },
+    });
+
     app.stack(API);
-  }
+  },
 } satisfies SSTConfig;
