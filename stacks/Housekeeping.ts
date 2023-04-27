b6a82acc2ea72e2277a67e9034d3233ed9321d1a
Matthew Wyskiel <developer@mattwyskiel.com>
2023-04-27 16:16:28 -0400

    fix cleanup job cron rate

--- stacks/Housekeeping.ts
+++ stacks/Housekeeping.ts
@@ -5,7 +5,7 @@ export const Housekeeping = ({ stack }: StackContext) => {
   const { bucket, DATABASE_URL } = use(Infra);
   const cleanupReceiptDocuments = new Cron(stack, 'CleanupReceiptDocuments', {
     job: 'packages/functions/src/housekeeping/cleanup-receipt-documents.handler',
-    schedule: 'rate(1 week)',
+    schedule: 'rate(7 days)',
   });
   cleanupReceiptDocuments.bind([bucket, DATABASE_URL]);
 };
