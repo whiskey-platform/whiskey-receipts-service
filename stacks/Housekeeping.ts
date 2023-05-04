6c2ba60924063baf2cfd04cb12128b4ae8d708ac
Matthew Wyskiel <developer@mattwyskiel.com>
2023-05-04 16:48:40 -0400

    increase receipt cleanup timeout

--- stacks/Housekeeping.ts
+++ stacks/Housekeeping.ts
@@ -5,7 +5,12 @@ import { Tags } from 'aws-cdk-lib';
 export const Housekeeping = ({ stack }: StackContext) => {
   const { bucket, DATABASE_URL } = use(Infra);
   const cleanupReceiptDocuments = new Cron(stack, 'CleanupReceiptDocuments', {
-    job: 'packages/functions/src/housekeeping/cleanup-receipt-documents.handler',
+    job: {
+      function: {
+        handler: 'packages/functions/src/housekeeping/cleanup-receipt-documents.handler',
+        timeout: '5 minutes',
+      },
+    },
     schedule: 'rate(7 days)',
   });
   cleanupReceiptDocuments.bind([bucket, DATABASE_URL]);
