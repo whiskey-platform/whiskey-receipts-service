cb7cd5ef634989849e9c82e0637f4274943d5314
Matthew Wyskiel <developer@mattwyskiel.com>
2023-05-18 09:44:33 -0400

    fix time string for document ingest

--- packages/functions/src/handle-ingest-event.ts
+++ packages/functions/src/handle-ingest-event.ts
@@ -58,8 +58,8 @@ export const handler: SNSHandler = async event => {
         sourceBucket: Bucket.ReceiptsBucket.bucketName,
         sourceKey: `${id}.${extension(input.documentType)}`,
         destinationKey: `Finances/Receipts/${datetime.year}/${datetime.toFormat(
-          'yyyy-MM-dd hh:mm:ss'
-        )} - ${input.store}.pdf`,
+          'yyyy-MM-dd'
+        )} ${datetime.toLocaleString(DateTime.TIME_SIMPLE)} - ${input.store} (${id}).pdf`,
       },
       Topic.DocumentIngestTopic.topicArn
     );
